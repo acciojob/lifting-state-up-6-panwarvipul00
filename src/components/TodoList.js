ee2de13b17848e421c036a4681449c7713e7db09
panwarvipul00 <panwarvipul1234@gmail.com>
2025-06-26 12:49:09 +0530

    third

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function TodoList({ todos, onComplete }) {
   return (
     <ul>
-      {todos.map(todo => (
-        <li key={todo.id} style={{ marginBottom: '10px' }}>
-          <span
-            style={{
-              textDecorationLine: todo.completed ? 'line-through' : 'none',
-              marginRight: '10px'
-            }}
-          >
+      {todos.map((todo) => (
+        <li key={todo.id} style={{ marginBottom: "10px" }}>
+          <span style={{ marginRight: "10px" }}>
             {todo.task}
           </span>
           {!todo.completed && (
